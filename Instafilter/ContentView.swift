3114345645f09154952d8bb085a291610fbdaae2
Omnivrr <Omnivrr@gmail.com>
2023-08-05 12:20:10 +0100

    Confirmation Dialog

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -8,12 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingConfirmation = true
+    @State private var backgroundColor = Color.white
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+                .frame(width: 300, height: 300)
+                .background(backgroundColor)
+                .onTapGesture {
+                    showingConfirmation = true
+                }
+                .confirmationDialog("Change background", isPresented: $showingConfirmation) {
+                    Button("Red") {backgroundColor = .red}
+                    Button("Green") { backgroundColor = .green}
+                    Button("Blue") { backgroundColor = .blue}
+                    Button("Cancel", role: .cancel) {}
+                }
         }
         .padding()
     }
