75c650577a3d18376a8011fbb8dfa225c9b71441
Omnivrr <Omnivrr@gmail.com>
2023-08-06 11:37:42 +0100

    Image selection updated

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -16,6 +16,9 @@ struct ContentView: View {
     @State private var showingImagePicker = false
     @State private var inputImage: UIImage?
     
+    @State private var currentFilter = CIFilter.sepiaTone()
+    let context = CIContext()
+    
     var body: some View {
         VStack {
             ZStack {
@@ -62,12 +65,26 @@ struct ContentView: View {
     
     func loadImage() {
         guard let inputImage = inputImage else { return }
-        image = Image(uiImage: inputImage)
+        
+       let beginImage = CIImage(image: inputImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        applyProcessing()
         
     }
     
     func save() {
         
+    }
+    
+    func applyProcessing() {
+        currentFilter.intensity = Float(filterIntensity)
+        
+        guard let outputImage = currentFilter.outputImage else { return }
+         
+        if let cgimg = context .createCGImage(outputImage, from:  outputImage.extent) {
+            let uiImage = UIImage(cgImage: cgimg)
+            image = Image(uiImage: uiImage)
+        }
     }
 }
 
